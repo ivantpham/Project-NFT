2dce34601ffda3c10b8d0a4c19efab4c4a09daa2
Phạm Lợi <phamloi@Phams-MacBook-Pro-2.local>
2024-07-19 20:19:48 +0700

    push final

--- frontend/src/contract-api/addressContractNFT.js
+++ frontend/src/contract-api/addressContractNFT.js
@@ -1,2 +1,2 @@
-const addressContractNFT = "0x786E04aB7D2EE09CfF60dE451319Ff539da4dF20";
+const addressContractNFT = "0x3D8b68Ec264659Eff9F59273e50500886F14dD0e";
 export default addressContractNFT;
